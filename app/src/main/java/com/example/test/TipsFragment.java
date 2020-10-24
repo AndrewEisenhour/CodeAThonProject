4236b295550bbe5a745f78257efe12bfcd9d0efc
aseis <a.eisenhour@aol.com>
2020-10-24 15:34:47 -0500

    Minor updates, text changes and formatting

--- app/src/main/java/com/example/test/TipsFragment.java
+++ app/src/main/java/com/example/test/TipsFragment.java
@@ -17,7 +17,7 @@ public class TipsFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tips2, container, false);
+        return inflater.inflate(R.layout.fragment_tips, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
