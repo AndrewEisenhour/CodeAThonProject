18d90e1e6e9d72261728a01b537db9b1979dcf08
aseis <a.eisenhour@aol.com>
2020-10-24 11:54:19 -0500

    Did a lot of stuff

--- app/src/main/java/com/example/test/ThirdFragment.java
+++ app/src/main/java/com/example/test/ThirdFragment.java
@@ -9,32 +9,24 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
-
+public class ThirdFragment extends Fragment {
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_third, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second_previous).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_third_previous).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
-        view.findViewById(R.id.button_second_next).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.secondToThird);
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
     }
