a8f6076b8fd60840e6636bc60e9b6263675a1360
aseis <a.eisenhour@aol.com>
2020-10-24 13:38:27 -0500

    Created the framework for the peppermint and chai path, not implemented

---
+++ app/src/main/java/com/example/test/EighthFragment.java
@@ -0,0 +1,41 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class EighthFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_eighth, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_second_previous).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(EighthFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+        view.findViewById(R.id.button_second_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(EighthFragment.this)
+                        .navigate(R.id.secondToThird);
+            }
+        });
+    }
+}
